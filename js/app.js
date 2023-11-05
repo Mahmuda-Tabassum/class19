fc3b60ddf60f8d1d893459fd4760273b74c61e89
Mahmuda Tabassum <143349699+Mahmuda-Tabassum@users.noreply.github.com>
2023-11-05 06:37:59 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,21 @@
+let aside = document.querySelector("aside");
+
+document.querySelector("#aside-btn").addEventListener("click", () => {
+    aside.classList.toggle('hide-show')
+});
+
+icon9.addEventListener('click', () => {
+    document.documentElement.classList.toggle('dark');
+
+    if (document.documentElement.classList.contains('dark')) {
+        icon9.innerHTML = '<i class="fa-solid fa-sun"></i>';
+        localStorage.setItem('mode', 'dark');
+    } else {
+        icon9.innerHTML = '<i class="fa-solid fa-moon"></i>';
+        localStorage.removeItem('mode');
+    }
+})
+
+if (localStorage.getItem('mode') == 'dark') {
+    document.documentElement.classList.add('dark');
+}
